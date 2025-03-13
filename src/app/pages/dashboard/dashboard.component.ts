e6c49c0db1c496869c792b2e74f0dfbff2319fb0
TEO <alfredo.ssm@gmail.com>
2025-03-13 01:07:41 -0300

    Se limpia dashboard ya no se ocupa material angular en dashboard solo bs

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,21 +1,17 @@
 import { Component } from '@angular/core';
 import { INDICATORS } from '../../constants/indicators';
 import { Router } from '@angular/router';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatList, MatListItem } from '@angular/material/list';
 
 @Component({
   selector: 'dashboard',
-imports: [MatCardModule, MatButtonModule, MatIconModule, MatList, MatListItem],
+  imports: [],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent {
   indicators = INDICATORS;
 
-  constructor(private readonly router: Router) { }
+  constructor(private readonly router: Router) {}
 
   listOfValue(idicatorId: string) {
     this.router.navigate(['/valores', idicatorId]);
@@ -24,5 +20,4 @@ export class DashboardComponent {
   detailsIndicator(idicatorId: string) {
     this.router.navigate(['/detalles', idicatorId]);
   }
-
 }
